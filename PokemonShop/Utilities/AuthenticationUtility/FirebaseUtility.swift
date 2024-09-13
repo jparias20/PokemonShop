488c98db519870ac9701c75c9eec47063c2d293c
Jhonatan Pulgarin Arias <jparias0731@gmail.com>
2024-09-13 10:53:58 -0500

    [Feature] - Sign up

--- PokemonShop/Utilities/AuthenticationUtility/FirebaseUtility.swift
+++ PokemonShop/Utilities/AuthenticationUtility/FirebaseUtility.swift
@@ -3,6 +3,15 @@ import FirebaseAuth
 import Foundation
 
 final class FirebaseUtility: AuthenticationUtility {
+    func signUp(_ email: String, password: String) async throws -> User {
+        do {
+            let response: AuthDataResult = try await Auth.auth().createUser(withEmail: email, password: password)
+            return User(email: email, uid: response.user.uid)
+        } catch {
+            throw AuthenticationUtilityError(firbaseCode: error._code)
+        }
+    }
+    
     func signIn(_ email: String, password: String) async throws -> User {
         do {
             let response: AuthDataResult = try await Auth.auth().signIn(withEmail: email, password: password)
